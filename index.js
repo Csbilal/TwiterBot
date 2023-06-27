0524d12001a837515bccf2d7ed6b6221a8e3e601
Csbilal <87218930+Csbilal@users.noreply.github.com>
2023-06-27 14:55:08 +0500

    chnges

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const { TwitterApi } = require("twitter-api-v2");
 const CronJob = require("cron").CronJob;
+const express = require("express");
+
+const app = express();
 
 const client = new TwitterApi({
   appKey: process.env.API_KEY,
@@ -9,34 +12,29 @@ const client = new TwitterApi({
   accessSecret: process.env.SECRET_TOKEN,
 });
 
-// const bearer = new TwitterApi(process.env.BEARER_TOKEN);
-
 const twitterClient = client.readWrite;
-// const twitterBearer = bearer.readOnly;
 
 const tweet = async () => {
   try {
-    await twitterClient.v2.tweet("this is new tweeet afatr half hoaur");
+    await twitterClient.v2.tweet("This is a new tweet after half an hour!");
   } catch (e) {
     console.log(e);
   }
 };
-// tweet();
 
-// const getUser = async () => {
-//   try {
-//     const res = await twitterClient.v2.user("Muhammad Bilal");
-//     console.log(res, "user");
-//   } catch (e) {
-//     ussr;
-//     console.log(e);
-//   }
-// };
-
-// getUser();
-
-const cronTweet = new CronJob("0,30 * * * *", async () => {
+const cronTweet = new CronJob("0 */30 * * * *", async () => {
   tweet();
 });
 
 cronTweet.start();
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("home Page");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
